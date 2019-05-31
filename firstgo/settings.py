f584f601f3a1ad678f4211d64298bdc68fe675f1
YoonaKim25 <oliviakim0205@gmail.com>
2019-06-01 00:31:37 +0900

    다시

--- firstgo/settings.py
+++ firstgo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ('DJANGO_SECRET_KEY', 'c&fsdo00o*wo(tm#3(a2%zvw4l$&tl_x^u
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
